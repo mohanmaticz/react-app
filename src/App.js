2142dcaf6a4080e3aea540a03ec3d5032a12431e
mohanmaticz <mohankumarmaticz@gmail.com>
2024-07-05 15:51:14 +0530

    feat: button component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Button/>
     </div>
   );
 }
